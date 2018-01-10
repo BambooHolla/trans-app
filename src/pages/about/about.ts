737b3854d0b1297c0a61db48c53cd578a2c1ae5b
ZYC <zhengyongchao@bnqkl.cn>
2018-01-10 18:47:18 +0800

    关于我们

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -8,6 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  commandData: any[] = [
+    {
+      icon: "gemail",
+      name: 'bngj@bnqkl.com',
+    }, {
+      icon: "gphone",
+      name: '0591-87275881',
+    }, {
+      icon: "gcompany",
+      name: '厦门本能管家科技有限公司',
+    }, {
+      icon: "gaddress",
+      name: '厦门市思明区湖滨北路72号中闽大厦38楼',
+    },
+  ]
+
   constructor(public navCtrl: NavController) {
 
   }
