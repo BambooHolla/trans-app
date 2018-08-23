02d070c34128bf74e95b9758b1434140da44424b
LSY <13295927826@163.com>
2018-08-23 15:20:18 +0800

    充值限额接口

--- src/pages/recharge-detail/recharge-detail.ts
+++ src/pages/recharge-detail/recharge-detail.ts
@@ -43,7 +43,7 @@ export class RechargeDetailPage extends SecondLevelPage {
     ) {
         super(navCtrl, navParams);
         this.productInfo = this.navParams.get("productInfo");
-        stockDataService.getProductStatus(this.productInfo.productId); 
+        stockDataService.getProductStatus(this.productInfo.productId);  
         this.userLanguage = appDataService.LANGUAGE || "zh";
     }
     productInfo: ProductModel = {} as any;
@@ -136,11 +136,11 @@ export class RechargeDetailPage extends SecondLevelPage {
     // @asyncCtrlGenerator.error('获取充值限额出错')
     async _getLimitedQuota() {
         return await this.accountService
-            .getLimitedQuota(this.productInfo.productHouseId, "001")
+            .getLimitedQuota(this.productInfo.productId, "001")
             .then(data => {
                 this.minRechargeText = "";
-                this.minRechargeText  += data[0] && data[0].min ? `最小充值金额是：${data[0].min}。`: '';
-                this.minRechargeText  += data[0] && data[0].max ? `最大充值金额是：${data[0].min}。`: '';
+                this.minRechargeText  += data[0] && data[0].min ? `最小充值数额是：${data[0].min}。`: '';
+                this.minRechargeText  += data[0] && data[0].max ? `最大充值数额是：${data[0].min}。`: '';
                 // if (data[0] && (this.productInfo.productDetail||this.productInfo.productName)) {
                 //     if (data[0].min && data[0].max) {
                 //         this.minRechargeText = `${
