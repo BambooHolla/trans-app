be696dd225825bd51ac46e28fee56fbbd0e3c9dc
LSY <13295927826@163.com>
2018-09-14 10:39:57 +0800

    V1.2.8

--- src/providers/app-data-service.ts
+++ src/providers/app-data-service.ts
@@ -26,7 +26,7 @@ export class AppDataService {
         serial: "",
     };
     //版本校验
-    public APP_VERSION = "v1.2.7";
+    public APP_VERSION = "v1.2.8";
     // 经纬度
     public GEOLOCATION: any = {
         latitude: "",
